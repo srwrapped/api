04b4e44e6178fc890bda299cf9ca01abe19231ba
Ikhbal Dwiyantoro <ikhbal.d@smt-id.com>
2023-11-30 22:27:57 +0700

    update wrapped method

--- routes/wrappedRoute.js
+++ routes/wrappedRoute.js
@@ -3,7 +3,7 @@ const wrappedController = require('../controller/wrapped');
 
 const router = express.Router();
 
-router.post('/most-watch', wrappedController.getMostWatchRoom);
-router.post('/premium-lives', wrappedController.getPremiumLiveHistory);
+router.get('/most-watch', wrappedController.getMostWatchRoom);
+router.get('/premium-lives', wrappedController.getPremiumLiveHistory);
 
 module.exports = router;
